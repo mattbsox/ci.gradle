de7a45dd49c0acfb1369103037b05aacfcc83432
mattbsox <m.bowersox@ibm.com>
2019-03-21 10:59:26 -0500

    fixing build commands

--- travis/build.sh
+++ travis/build.sh
@@ -2,4 +2,4 @@
 set -e
 
 export GRADLE_OPTS="-Dorg.gradle.daemon=true -Dorg.gradle.jvmargs='-XX:MaxPermSize=1024m -XX:+CMSClassUnloadingEnabled -XX:+UseConcMarkSweepGC -XX:+HeapDumpOnOutOfMemoryError -Xmx2048m'"
-gradlew.bat install check -Druntime=$RUNTIME -DruntimeVersion=$RUNTIME_VERSION --stacktrace --info --no-daemon
+./gradlew install check -Druntime=$RUNTIME -DruntimeVersion=$RUNTIME_VERSION --stacktrace --info --no-daemon
